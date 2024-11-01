56dd2c946b87372341b7933481768901366c4839
Chike Ode <chikeodenigbo@gmail.com>
2024-10-31 23:03:42 -0400

    changing color in UI dark mode

--- app.py
+++ app.py
@@ -189,6 +189,195 @@
 # if __name__ == "__main__":
 #     main()
 
+# import streamlit as st
+# from langchain import hub
+# from langchain_community.vectorstores import FAISS, Chroma
+# from langchain_community.document_loaders import PyPDFLoader
+# from langchain_core.output_parsers import StrOutputParser
+# from langchain_core.runnables import RunnablePassthrough
+# from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain_core.prompts import PromptTemplate
+# import os
+
+# st.set_page_config(
+#     page_title="Éko",
+#     page_icon="logo.png"  # Use .ico format for best compatibility
+# )
+
+# st.markdown(
+#     """
+#     <style>
+#     .stChatMessage {
+#         background-color: #f0f0f0;
+#         border-radius: 5px;
+#         padding: 10px;
+#     }
+#     .stSidebar {
+#         background-color: #f7f7f7;
+#         border-right: 2px solid #e0e0e0;
+#     }
+#     body {
+#         font-family: 'Arial', sans-serif;
+#     }
+#     .logo {
+#         display: block;
+#         margin: 0 auto;
+#         opacity: 0;
+#         animation: fadeIn 2s forwards;
+#     }
+#     .credit {
+#         font-size: 12px;  /* Smaller font size */
+#         text-align: left;  /* Left aligned */
+#         color: rgba(0, 0, 0, 0.8);  /* Faded color */
+#         margin-bottom: 20px;  /* Space below the credit */
+#     }
+#     @keyframes fadeIn {
+#         to {
+#             opacity: 1;
+#         }
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+# # Load logo
+# logo_url = "logo.png"  # Update with your logo path or URL
+
+# # Display logo with animation
+# st.sidebar.image(logo_url, caption="Bâtir des communautés inclusives", width=300)
+
+# st.title("Éko")
+# st.markdown(
+#     """
+#     <div class="credit">
+#         Built by <a href="https://www.chikeodenigbo.com" target="_blank">Chike Odenigbo</a>, Senior AI Systems Engineer
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
+# st.header("Obtenez des réponses sur l’itinérance dans votre région.")
+
+# def main():
+#     # Sample data for language and boroughs
+#     language = [
+#         "Français",
+#         "English"
+#     ]
+
+#     boroughs = [
+#         "Le Sud Ouest",
+#         "Cote-des-Neiges/Notre-Dame-de-Grace",
+#         "Ville-Marie", 
+#         "Le Plateau Mont-Royal", 
+#         "Mercier-Hochelaga-Maisonneuve"
+#     ]
+
+#     # Create a form for user input in the sidebar
+#     with st.sidebar.form(key='api_key_form'):
+#         st.header("OpenAI API Key")
+#         pword = st.text_input("OpenAI API Key:", type="password")
+        
+#         # Shelter selection
+#         selected_language = st.selectbox("Language:", language)
+            
+#         # Borough selection
+#         selected_borough = st.selectbox("Location:", boroughs)
+
+#         submit_button = st.form_submit_button(label='Submit')
+
+#     # Check if the form was submitted
+#     if submit_button or len(pword) > 0:
+#         # Set the API key based on user input
+#         if pword == "test":
+#             os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"]
+#             openai_api_key = st.secrets["OPENAI_API_KEY"]
+#         else:
+#             os.environ['OPENAI_API_KEY'] = pword
+#             openai_api_key = pword
+
+#         # Initialize the language model with the provided API key
+#         llm = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
+
+#         # Load documents
+#         file_path = "BENOIT LABRE.pdf"  # Update with your actual file path
+#         loader = PyPDFLoader(file_path)
+#         docs = loader.load()
+
+#         # Split documents for processing
+#         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+#         splits = text_splitter.split_documents(docs)
+
+#         # Initialize the vector store
+#         vectorstore = FAISS.from_documents(splits, embedding=OpenAIEmbeddings(api_key=openai_api_key))
+
+#         # Set up the retriever and prompt template
+#         retriever = vectorstore.as_retriever()
+
+#         prompt_templates = {
+#             "Français": PromptTemplate(
+#                 input_variables=["context", "question"],
+#                 template=(
+#                     "Vous êtes un expert de l’itinérance à Montréal et vous entendez des plaintes sur les itinérants par les personnes qui vivent dans la communauté. En fonction du contexte fourni et de vos connaissances, veuillez répondre à la question suivante brièvement:\n\n"
+#                     "Contexte : {context}\n\n"
+#                     "Question : {question}\n\n"
+#                     "Veuillez fournir une réponse empathique aux préoccupations des sans-abri et de la personne qui pose la question quand la question est chargée d'émotion. Si la question n’est pas liée a l'itinerance repondu normalement sans trop d'empathie et mention de l'itinerance, renseignez-vous davantage sur leurs préoccupations."
+#                 )
+#             ),
+#             "English": PromptTemplate(
+#                 input_variables=["context", "question"],
+#                 template=(
+#                     "You are an expert on homelessness in Montreal and you hear complaints about homeless people from residents of the community. Based on the provided context and your knowledge, please answer the following question concisely:\n\n"
+#                     "Context: {context}\n\n"
+#                     "Question: {question}\n\n"
+#                     "Please provide an empathetic response for both the homeless and the inquirer's concerns when the question is emotionally charged. If the question is not related to homelessness answer normally without need to be over empathetic inquire more on their concerns."
+#                 )
+#             )
+#         }
+
+#         # Select the appropriate prompt template based on the chosen language
+#         prompt_template = prompt_templates[selected_language]
+        
+#         def format_docs(docs):
+#             return "\n\n".join(doc.page_content for doc in docs)
+
+#         rag_chain = (
+#             {"context": retriever | format_docs, "question": RunnablePassthrough()}
+#             | prompt_template
+#             | llm
+#             | StrOutputParser()
+#         )
+
+#         # Initialize chat history
+#         if "messages" not in st.session_state:
+#             st.session_state.messages = []
+
+#         # Display chat messages from history on app rerun
+#         for message in st.session_state.messages:
+#             with st.chat_message(message["role"]):
+#                 st.markdown(message["content"])
+
+#         # React to user input
+#         if question := st.chat_input("Partagez vos préoccupations ici"):
+#             st.chat_message("user").markdown(question)
+#             st.session_state.messages.append({"role": "user", "content": question})
+
+#             response = rag_chain.invoke(question)
+            
+#             with st.chat_message("assistant"):
+#                 st.markdown(response)
+#             st.session_state.messages.append({"role": "assistant", "content": response})
+
+#     else:
+#         if selected_language == 'English':
+#             st.warning("Please enter your OpenAI API Key to use the app.")
+#         else:
+#             st.warning("Veuillez saisir votre clé API OpenAI pour utiliser l’application.")
+
+# if __name__ == "__main__":
+#     main()
+
 import streamlit as st
 from langchain import hub
 from langchain_community.vectorstores import FAISS, Chroma
@@ -208,17 +397,39 @@ st.set_page_config(
 st.markdown(
     """
     <style>
+    :root {
+        --background-color: #ffffff;
+        --text-color: #000000;
+        --chat-bg-color: #f0f0f0;
+        --sidebar-bg-color: #f7f7f7;
+        --border-color: #e0e0e0;
+        --credit-color: rgba(0, 0, 0, 0.8);
+    }
+
+    [data-theme='dark'] {
+        --background-color: #1e1e1e;
+        --text-color: #ffffff;
+        --chat-bg-color: #2d2d2d;
+        --sidebar-bg-color: #2a2a2a;
+        --border-color: #444444;
+        --credit-color: rgba(255, 255, 255, 0.8);
+    }
+
     .stChatMessage {
-        background-color: #f0f0f0;
+        background-color: var(--chat-bg-color);
         border-radius: 5px;
         padding: 10px;
+        color: var(--text-color);
     }
     .stSidebar {
-        background-color: #f7f7f7;
-        border-right: 2px solid #e0e0e0;
+        background-color: var(--sidebar-bg-color);
+        border-right: 2px solid var(--border-color);
+        color: var(--text-color);
     }
     body {
         font-family: 'Arial', sans-serif;
+        background-color: var(--background-color);
+        color: var(--text-color);
     }
     .logo {
         display: block;
@@ -229,7 +440,7 @@ st.markdown(
     .credit {
         font-size: 12px;  /* Smaller font size */
         text-align: left;  /* Left aligned */
-        color: rgba(0, 0, 0, 0.8);  /* Faded color */
+        color: var(--credit-color);  /* Faded color */
         margin-bottom: 20px;  /* Space below the credit */
     }
     @keyframes fadeIn {
@@ -318,20 +529,20 @@ def main():
         prompt_templates = {
             "Français": PromptTemplate(
                 input_variables=["context", "question"],
-                template=(
+                template=(  # Prompt template for French
                     "Vous êtes un expert de l’itinérance à Montréal et vous entendez des plaintes sur les itinérants par les personnes qui vivent dans la communauté. En fonction du contexte fourni et de vos connaissances, veuillez répondre à la question suivante brièvement:\n\n"
                     "Contexte : {context}\n\n"
                     "Question : {question}\n\n"
-                    "Veuillez fournir une réponse empathique aux préoccupations des sans-abri et de la personne qui pose la question quand la question est chargée d'émotion. Si la question n’est pas liée a l'itinerance repondu normalement sans trop d'empathie et mention de l'itinerance, renseignez-vous davantage sur leurs préoccupations."
+                    "Veuillez fournir une réponse empathique aux préoccupations des sans-abri et de la personne qui pose la question quand la question est chargée d'émotion. Varie ton niveau d'empathie dependent du niveau d'emotion dans la question. Si la question n’est pas liée a l'itinerance repondu normalement sans trop d'empathie et mention de l'itinerance, renseignez-vous davantage sur leurs préoccupations."
                 )
             ),
             "English": PromptTemplate(
                 input_variables=["context", "question"],
-                template=(
+                template=(  # Prompt template for English
                     "You are an expert on homelessness in Montreal and you hear complaints about homeless people from residents of the community. Based on the provided context and your knowledge, please answer the following question concisely:\n\n"
                     "Context: {context}\n\n"
                     "Question: {question}\n\n"
-                    "Please provide an empathetic response for both the homeless and the inquirer's concerns when the question is emotionally charged. If the question is not related to homelessness answer normally without need to be over empathetic inquire more on their concerns."
+                    "Please provide an empathetic response for both the homeless and the inquirer's concerns when the question is emotionally charged. Vary your level of empathy based on the emotion level in the question. If the question is not related to homelessness answer normally without need to be over empathetic inquire more on their concerns."
                 )
             )
         }
